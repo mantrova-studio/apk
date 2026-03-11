e605d6ec821efcefbd6f70274e968d68221c2241
mantrova-studio <260282153+mantrova-studio@users.noreply.github.com>
2026-03-11 14:36:46 +0500

    Delete app directory

--- app/src/main/java/com/polihov/restaurantshell/Restaurant.kt
+++
@@ -1,9 +0,0 @@
-package com.polihov.restaurantshell
-
-data class Restaurant(
-    val id: Int,
-    val name: String,
-    val url: String,
-    val logoResId: Int,
-    val placeholderNote: String = ""
-)
