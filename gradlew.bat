50d52c14f0da23492d7246784c08092a5998a94d
mantrova-studio <260282153+mantrova-studio@users.noreply.github.com>
2026-03-11 14:37:35 +0500

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DIR=%~dp0
-SET CLASSPATH=%DIR%\gradle\wrapper\gradle-wrapper.jar
-IF DEFINED JAVA_HOME (
-  SET JAVACMD=%JAVA_HOME%\bin\java.exe
-) ELSE (
-  SET JAVACMD=java.exe
-)
-"%JAVACMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
